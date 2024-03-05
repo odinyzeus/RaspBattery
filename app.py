b3305705b997cc8a3f3727989f925f6a1a6412c4
Eduardo Vargas <odinyzeus@gmail.com>
2024-03-05 12:24:19 -0600

    Example code update

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ import Thermography as tg
 import guiApp as gui
 
 if __name__ == "__main__":
-    app = gui.ventana_principal
-    aplicacion = gui.AplicacionTkinter(app)
-    app.mainloop()
+    app = gui.ApplicationTkinter()
--- guiApp.py
+++ guiApp.py
@@ -1,50 +1,117 @@
-import tkinter as tk
+#from tkinter import *
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
-class AplicacionTkinter:
-    total_columns = 3
-    def __init__(self, ventana_principal:tk):        
-        # the principal window parameters are setting here
-        self.ventana_principal = ventana_principal
-        # Establecer dimensiones mínimas de la ventana principal
-        self.ventana_principal.minsize(width=800, height=600) 
-        self.ventana_principal.title("Digital Lock-In Amplifier for Thermography Applications")
-        self.ventana_principal.resizable(True,True)
-        self.ventana_principal.bind("<Configure>", self.adjust_content)
-        
-        # Makes the application header 
-        self.frame_header = tk.Frame(self.ventana_principal,height= 150,padx=5,pady=5)
-        self.frame_header.pack(expand=True,fill=tk.BOTH) 
-        self.frame_header.grid(row=0, column=0, columnspan=self.total_columns, padx=5, pady=5)
+class ApplicationTkinter:
+    total_columns = 3               # Defines the maximum number of columns of app's grid
+    Win: any
+    Title:str
+    wpadX = 3
+    wpadY = 3
+    border = 2
     
-   
-   
-   
-        # Crear la cuadrícula de 4 celdas
-        self.frame_camara_usb = tk.Frame(self.ventana_principal, bg="lightblue", padx=10, pady=10)
-        self.frame_camara_usb.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+    def __init__(self, theme:str = "superhero") -> None:
+        # the principal window parameters are setting here
+        self.Win = ttk.Window(themename=theme)      # Defines the graphic window and theme for app's GUI
+        self.Win.title('Digital Lock-In Amplifier for Thermography Applications')       
+        #self.Win.iconbitmap()
+        self.Win.minsize(width=800, height=600)     # Set minsize of GUI
+        self.Win.resizable(True,True)               # The App can be resized
         
-        self.frame_video_archivo = tk.Frame(self.ventana_principal, bg="lightgreen", padx=10, pady=10)
-        self.frame_video_archivo.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
-        
-        self.frame_campos_texto = tk.Frame(self.ventana_principal, bg="lightyellow", padx=10, pady=10)
-        self.frame_campos_texto.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
-        
-        self.frame_info_sistema = tk.Frame(self.ventana_principal, bg="lightcoral", padx=10, pady=10)
-        self.frame_info_sistema.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
-
-        # Barra de estado
-        self.barra_estado = tk.Label(self.ventana_principal, text="Barra de estado", bd=1, relief=tk.SUNKEN, anchor=tk.W)
-        self.barra_estado.grid(row=3, columnspan=2, sticky="ew")
-        
-        # Menú principal
-        self.menu_principal = tk.Menu(self.ventana_principal)
+        # Makes the application menu
+        self.menu_principal = ttk.Menu(self.Win)
         self.menu_principal.add_command(label="Archivo", command=self.dummy)
         self.menu_principal.add_command(label="Editar", command=self.dummy)
         self.menu_principal.add_command(label="Ayuda", command=self.dummy)
-        self.ventana_principal.config(menu=self.menu_principal)
+        self.Win.config(menu=self.menu_principal)
+        
+        # Creates principal frame 
+        self.main_frame = ttk.Frame(self.Win)
+        self.main_frame.pack(side=TOP, fill=BOTH, expand=True)
+        
+        # Creates status bar frame
+        self.statusbar = ttk.Label(self.Win, text="Estado: Listo", style="info.TLabel")
+        self.statusbar.pack(side=BOTTOM,fill=X,expand=True)
+        #self.statusbar.grid(row=3, column=0, columnspan=self.total_columns, sticky=EW,pady=(0, 5),ipady = 15)
+        
+    
+        self.frame_header = ttk.Frame(self.main_frame,bootstyle=SECONDARY,borderwidth=self.border)
+        self.frame_header.grid(row=0, column=0, columnspan=self.total_columns, sticky=EW)
+        #self.frame_header.pack(side=ttk.TOP, fill=ttk.BOTH, expand=True, padx=self.wpadX, pady=self.wpadY)
+        
+        self.frame_body = ttk.Frame(self.main_frame, bootstyle=SECONDARY,borderwidth=self.border)
+        self.frame_body.grid(row=1, column=0, columnspan=self.total_columns, sticky=EW)
+        #self.frame_body.pack(side=ttk.TOP, fill=ttk.BOTH, expand=True, padx=self.wpadX, pady=self.wpadY)
+        
+        self.frame_footer = ttk.Frame(self.main_frame,bootstyle=SECONDARY,borderwidth=self.border)
+        self.frame_footer.grid(row=2, column=0, columnspan=self.total_columns, sticky=EW)
+        #self.frame_footer.pack(side=ttk.TOP, fill=ttk.BOTH, expand=True, padx=self.wpadX, pady=self.wpadY)    
+        
+        
+        
+        self.add_header()
+        
+        self.Win.mainloop()
+
+    def add_header(self):
+        # Crear el frame para el header
+        pass
+
+    def dummy(self):
+        pass
+
+"""
+class AplicacionTkinter:
+    
+    Win = tk.Tk()                   
+    
+    def __init__(self):                
+                     
+        self.Win.resizable(True,True)                         
+        self.Win.bind("<Configure>", self.adjust_content)     # bind "configure" event to adjust's method
+        
+        
+        
+        # Makes the application header section 
+        self.frame_header = tk.Frame(self.Win,height= 150,padx=self.PadX,pady=self.PadY) 
+        self.frame_header.grid(row=0, column=0, columnspan=self.total_columns, padx=self.PadX, pady=self.PadY)
+        self.add_border(self.frame_header)
+        
+       
+        self.Win.mainloop()
+        
+        
+    def add_body(self):
+        _summary_
+        This function makes the body appeareance and format desired
+        
+        
+        self.frame_camara_usb = tk.Frame(self.frame_body, bg="lightblue", padx=self.PadX, pady=self.PadY)
+        self.frame_camara_usb.grid(row=0, column=0, padx=self.PadX, pady=self.PadY, sticky="nsew")
+        self.add_border(self.frame_camara_usb)
+        
+        self.frame_video_archivo = tk.Frame(self.frame_body, bg="lightgreen", padx=self.PadX, pady=self.PadY)
+        self.frame_video_archivo.grid(row=1, column=0, padx=self.PadX, pady=self.PadY, sticky="nsew")
+        self.add_border(self.frame_video_archivo)
+        
+        self.frame_campos_texto = tk.Frame(self.frame_body, bg="lightyellow", padx=self.PadX, pady=self.PadY)
+        self.frame_campos_texto.grid(row=0, column=1, padx=self.PadX, pady=self.PadY, sticky="nsew")
+        self.add_border(self.frame_campos_texto)
+        
+        self.frame_info_sistema = tk.Frame(self.frame_body, bg="lightcoral", padx=self.PadX, pady=self.PadY)
+        self.frame_info_sistema.grid(row=1, column=1, padx=self.PadX, pady=self.PadY, sticky="nsew")
+        self.add_border(self.frame_info_sistema)
 
         # Agregar contenido a los frames
         self.add_content()
+        
+
+    def add_border(self, frame):
+    # Agregar un borde alrededor del frame
+        border = tk.Frame(frame, borderwidth=2, relief="groove")
+        border.pack(expand=True, fill="both")
+
+        
         
     def add_content(self):
         # Campos de texto para datos del sistema
@@ -65,14 +132,27 @@ class AplicacionTkinter:
 
     def adjust_content(self, event):
     # Adjust the size of application in real time
-        num_filas = self.ventana_principal.grid_size()[1] 
+        num_filas = self.Win.grid_size()[1] 
         for i in range(1, num_filas):  # Puedes ajustar este rango según la cantidad de filas en tu diseño
-            self.ventana_principal.grid_rowconfigure(i, weight=1)
+            self.Win.grid_rowconfigure(i, weight=1)
                
-        self.ventana_principal.grid_columnconfigure(0, weight=1)
+        self.Win.grid_columnconfigure(0, weight=1)
 
 
-    def dummy(self):
-        pass
-    
-ventana_principal = tk.Tk()
+    def dany(self):
+         # Makes the application body section
+        self.frame_body   = tk.Frame(self.Win,padx=self.PadX,pady=self.PadY)
+        self.frame_body.grid(row=1, column=0, columnspan=self.total_columns, padx=self.PadX, pady=self.PadY)
+
+        # Makes the application footer section 
+        self.frame_footer  = tk.Frame(self.Win,padx=self.PadX,pady=self.PadY)
+        self.frame_footer.grid(row=2, column=0, columnspan=self.total_columns, padx=self.PadX, pady=self.PadY)
+
+        
+        
+        
+        self.add_body()
+
+
+        
+"""
